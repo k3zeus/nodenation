f40ce95e61b7e715eced836474c7123d47e65eee
K3zeus <132515920+k3zeus@users.noreply.github.com>
2025-07-03 01:39:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
+#!bin/bash
 # Start Ghost Nation Script v.01
+#
 echo "##################################"
 echo "## Welcome to Ghost Node Nation ##"
-echo "##@###############################"
+echo "##################################"
 
-sudo cd /root/
-
-#!bin/bash
-
+echo ""
 echo "Download Github Project"
 sudo git clone https://github.com/k3zeus/nodenation.git /root/nodenation
 find /root/nodenation/ -type f -name "*.sh" -print0 | xargs -0 dos2unix
 
 echo ""
 echo "#################################"
-echo "Change permition to scripts:"
+echo "# Change permition to scripts: #"
+echo "#################################"
 find /root/nodenation/ -name "*.sh" -type f -print0 | xargs -0 chmod +x
 
 # Execute the Menu:
