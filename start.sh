f218ddc457a14a0bb7e5c7a6c524616af60b5cc8
K3zeus <132515920+k3zeus@users.noreply.github.com>
2025-07-03 09:50:23 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,8 @@ echo "Download Github Project"
 sudo git clone https://github.com/k3zeus/nodenation.git /root/nodenation
 find /root/nodenation/ -type f -name "*.sh" -print0 | xargs -0 dos2unix
 
+echo " Changing permition to scripts: "
 echo ""
-echo "#################################"
-echo "# Change permition to scripts: #"
-echo "#################################"
 find /root/nodenation/ -name "*.sh" -type f -print0 | xargs -0 chmod +x
 
 # Execute the Menu:
