6133c264f7deae1e94a749660fedc061cc2154be
K3zeus <132515920+k3zeus@users.noreply.github.com>
2026-03-19 00:52:10 -0300

    Enhance install script with root check and dos2unix
    Added root user check and converted scripts to Unix format.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #
 # Script Halfin Install 
-
+#
+    if [ "$EUID" -ne 0 ]; then
+        echo "[ERRO] Este script precisa ser executado como root."
+        exit 1
+    fi
+#
+sudo find /home/ -type f -name "*.sh" -print0 | xargs -0 sudo dos2unix
 # Download do Projeto Completo
 wget https://github.com/k3zeus/nodenation/archive/refs/tags/beta_v2.tar.gz && tar -xzvf beta_v2.tar.gz
 
