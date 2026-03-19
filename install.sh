6752e1083bc61424929aeb91f0c980911e9fd8ff
K3zeus <132515920+k3zeus@users.noreply.github.com>
2026-03-19 00:53:14 -0300

    Update install script for Ghost Nodes v0.2

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 #
-# Script Halfin Install 
+# Script Halfin Install - Ghost Nodes v0.2 19032026
 #
     if [ "$EUID" -ne 0 ]; then
         echo "[ERRO] Este script precisa ser executado como root."
         exit 1
     fi
 #
-sudo find /home/ -type f -name "*.sh" -print0 | xargs -0 sudo dos2unix
 # Download do Projeto Completo
 wget https://github.com/k3zeus/nodenation/archive/refs/tags/beta_v2.tar.gz && tar -xzvf beta_v2.tar.gz
+
+#
+sudo find /home/ -type f -name "*.sh" -print0 | xargs -0 sudo dos2unix
 
 # Permissão de execusão para os scripts
 chmod nodenation-beta_v2/hal2026/*.sh
