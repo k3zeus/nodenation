55c4dea405e7471745857047c15c23000904a88e
K3zeus <132515920+k3zeus@users.noreply.github.com>
2026-03-21 23:49:38 -0300

    Recuperação do docker

--- hal2026/docker/docker-compose_update.sh
+++ hal2026/docker/docker-compose_update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo systemctl restart docker
+
 docker-compose pull
 docker-compose up -d
 
